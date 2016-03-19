90831023f4d3a31b8cf4ad3655f77e96442af510
Daigo Fujiwara <daigo@daigofujiwara.com>
2016-03-19 17:29:25 -0400

    Submit this

--- js/app.js
+++ js/app.js
@@ -1,1 +1,16 @@
-$(document).foundation();
+$(document).foundation().ready(function() {
+    console.log( "ready!" );
+
+    $('#hackbaseball').DataTable( {
+	    "ajax": '/js/salary.json',
+	    "columns": [
+            { "data": "NAME" },
+            { "data": "TEAM" },
+            { "data": "POS" },
+            { "data": "SALARY" },
+            { "data": "np" },
+            { "data": "dollar_per_pitch" }
+        ]
+    });
+
+});
