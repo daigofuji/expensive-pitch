5b64a384d6f484b7a34363101cee5fa5315257c1
Daigo Fujiwara <daigo@daigofujiwara.com>
2016-03-19 23:20:00 -0400

    Fixed sorting

--- js/app.js
+++ js/app.js
@@ -10,7 +10,21 @@ $(document).foundation().ready(function() {
             { "data": "SALARY" },
             { "data": "np" },
             { "data": "dollar_per_pitch" }
-        ]
-    });
+        ],
+        paging: false
+    }).order( [ 5, 'desc' ] )
+    .draw();
+
+    $('#did-not-pitch').DataTable( {
+	    "ajax": '/js/did-not-pitch.json',
+	    "columns": [
+            { "data": "NAME" },
+            { "data": "TEAM" },
+            { "data": "POS" },
+            { "data": "SALARY" }
+        ],
+        paging: false
+    }).order( [ 3, 'desc' ] )
+    .draw();
 
 });
